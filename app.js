60f3edf7fc245f69102342b718a66882aa561b69
MikArmy <esercitatomichele@gmail.com>
2022-02-05 21:25:22 -0700

    initial NodeJS and React setup

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.post("/post", (req, res) => {
+    console.log("React connected");
+    res.redirect("/");
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
