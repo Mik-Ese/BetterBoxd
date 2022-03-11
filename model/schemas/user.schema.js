a2ae8800004283c628c5a6d6cf674c28a2c6784e
ethangillengg <ethan.gill@ucalgary.ca>
2022-03-10 22:40:28 -0700

    add dummy data

--- model/schemas/user.schema.js
+++ model/schemas/user.schema.js
@@ -1,12 +1,12 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  // no "_id" field since it is automatically generated
-  username: String,
-  email_address: String,
-  password: String,
-  lists: [{ list_name: String, media_ids: [String] }],
+    // no "_id" field since it is automatically generated
+    username: String,
+    email_address: String,
+    password: String,
+    lists: [{ list_name: String, media_ids: [String] }],
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model('User', userSchema);
--- model/schemas/userRating.schema.js
+++ model/schemas/userRating.schema.js
@@ -1,10 +1,10 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userRatingSchema = new Schema({
-  user_id: Number,
-  trakt_id: Number,
-  rating: Number,
+    user_id: String,
+    trakt_id: Number,
+    rating: Number,
 });
 
-module.exports = mongoose.model("MediaRating", ratingSchema);
+module.exports = mongoose.model('MediaRating', ratingSchema);
