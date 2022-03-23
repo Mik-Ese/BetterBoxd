dcca8a698be233f115f3eff941f1dc80a79f2322
ethangillengg <ethan.gill@ucalgary.ca>
2022-03-22 20:41:53 -0600

    added models
    Added model schemas for movieList and journalEntry

---
+++ model/schemas/journalEntry.schema.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const journalEntrySchema = new Schema({
+    // no "_id" field since it is automatically generated
+    movieIds: [
+        {
+            trakt: Number,
+            slug: String,
+            imdb: String,
+            tmdb: Number
+        }
+    ],
+    userId: String,
+    rating: Number, //Rating that the user gave to the movie
+    content: String //Content of the journal entry
+});
+
+module.exports = mongoose.model('JournalEntry', journalEntrySchema);
