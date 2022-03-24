1b8f6a53dafb5025b519ebeaaa80e8bc965606bc
ethangillengg <81479108+ethangillengg@users.noreply.github.com>
2022-03-23 23:14:44 -0600

    Update userRating.schema.js

--- model/schemas/userRating.schema.js
+++ model/schemas/userRating.schema.js
@@ -1,10 +1,13 @@
-var mongoose = require('mongoose');
-const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const connection = require('../util/database');
+const User = require('./user.schema');
 
-const userRatingSchema = new Schema({
-    user_id: String,
-    trakt_id: Number,
-    rating: Number
+const userRating = new mongoose.Schema({
+    // no "_id" field since it is automatically generated
+    user_id: { type: mongoose.Schema.ObjectId, ref: User, required: true },
+    trakt_id: { type: Number, required: true },
+    rating: { type: Number, required: true }
 });
 
-module.exports = mongoose.model('MediaRating', ratingSchema);
+const UserRating = connection.model('UserRating', userRating);
+module.exports = UserRating;
