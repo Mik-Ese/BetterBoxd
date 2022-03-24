53b3f6ca2dd936c0181bc132679c922d136adb40
ethangillengg <81479108+ethangillengg@users.noreply.github.com>
2022-03-24 14:05:35 -0600

    Update DBfetch.js

--- model/DBfetch.js
+++ model/DBfetch.js
@@ -1,14 +1,33 @@
 const User = require('./schemas/user.schema');
 const UserReview = require('./schemas/userReview.schema');
+const JournalEntry = require('./schemas/journalEntry.schema');
+
+async function fetchById(id, model) {
+    var res = await model.findById(id).exec();
+    return res;
+}
 
 /**
  * @param {*} id - id of the user to fetch
  * @returns The user with that id
  */
 async function fetchUser(id) {
-    var user = await User.findById(id).exec();
-    console.log(user);
-    return user;
+    return await fetchById(id, User);
 }
 
+/**
+ * @param {*} id - id of the user to fetch
+ * @returns The user with that id
+ */
+async function fetchUserReview(id) {
+    return await fetchById(id, UserReview);
+}
+
+/**
+ * @param {*} id - id of the user to fetch
+ * @returns The user with that id
+ */
+async function fetchJournalEntry(id) {
+    return await fetchById(id, JournalEntry);
+}
 module.exports = { fetchUser };
