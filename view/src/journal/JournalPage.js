9c81a3aa91e152848c8b5c29247227dcb3e6ef33
Lucas-Ion <65054476+Lucas-Ion@users.noreply.github.com>
2022-03-24 23:36:05 -0600

    Broken Reviews Page

--- view/src/journal/JournalPage.js
+++ view/src/journal/JournalPage.js
@@ -3,28 +3,59 @@ import JournalReview from './JournalReview';
 
 const JournalPage = () => {
 
+const [JournalReviews, setJournalReview] = useState([
+  {
+    imgLink: "https://m.media-amazon.com/images/M/MV5BYTExZTdhY2ItNGQ1YS00NjJlLWIxMjYtZTI1MzNlMzY0OTk4XkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg",
+    title: "The Batman",
+    movie_review: "This was a really good movie, I thoroughly enjoyed it! I love the Batman"
+  },
+  {
+    imgLink: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/d1pklzbuyaab0la-1552597012.jpg",
+    title: "Avengers Endgame",
+    movie_review: "Greatest Marvel Movie ever!"
+
+  },
+  {
+    imgLink: "https://www.joblo.com/wp-content/uploads/2014/11/paddington_bear_ver6_xxlg-1.jpg",
+    title: "Paddington",
+    movie_review: "Cute Bear"
+  }
+
+])
+
+  const reviewFactory = () => {
+    
+    var journalEntries = [];
+    for (var i = 0; i < JournalReviews.length; i++) {
+        var review = JournalReviews[i];
+        journalEntries.push(<JournalReview {...{review}} />);
+    }
+
+    return journalEntries;
+   
+  };
+
   return (
     <div>
       <div className="Journal-Page-Header-Text">
         <h1 className="display-1 Journal-Review-Text text-primary">
-
           <span className="text-primary"> Your </span>  <span className="text-warning"> Movie </span>
-          <span className="text-success"> Journal </span>
+          <span className="text-success"> Journal </span> 
+          <div className ="my-journal-reviews">{reviewFactory()}</div>
+          <div class="d-inline-flex flex-row justify-content-center p-3">
+                <button type="button" class="btn-lg btn btn-primary m-4 p-3">Write a review</button>
+                <button type="button" class="btn-lg btn btn-success m-4 p-3">Toggle Next</button>
+            </div>
 
         </h1>
       </div>
-
-
-
-      <JournalReview />
-
-
-
-
-    </div>
-
-
+    </div> 
   );
+    
 };
+
+
+
+  
 
 export default JournalPage;
--- view/src/journal/JournalReview.js
+++ view/src/journal/JournalReview.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
-const JournalReview = () => {
+const JournalReview = (review) => {
+
 
     return (
         <div className="App">
@@ -8,35 +9,12 @@ const JournalReview = () => {
             <div class="d-inline-flex flex-row justify-content-center p-3">
                 <div class="card align-middle m-5" style={{ width: "25rem" }}>
                     <div class="card-body">
-                        <img src="https://coolhdwall.com/storage/202105/the-batman-movie-hd-phone-wallpaper-1080x1920.jpg" class="card-img-top rounded" style={{ height: "30rem" }} alt="..." />
-                        <h5 class="card-title display-5">The Batman</h5>
+                        <img src={review.imgLink} class="card-img-top rounded" style={{ height: "30rem" }} alt="..." />
+                        <div class="card-title display-5">{review.title}</div>
                         <p class="card-text text-primary">Your Review:</p>
-                        <p class="card text-md-dark ">Batman is cool, I really loved this movie! Gotham is portrayed in a totally different light very different to the Nolan films.</p>
+                        <div class="card text-md-dark ">{review.movie_review}</div>
                     </div>
                 </div>
-                <div class="card align-middle m-5" style={{ width: "25rem" }}>
-                    <div class="card-body">
-                        <img src="https://images-na.ssl-images-amazon.com/images/I/91D1tVKccBL.jpg" class="card-img-top rounded" style={{ height: "30rem" }} alt="..." />
-                        <h5 class="card-title display-5">The French Dispath</h5>
-                        <p class="card-text text-primary">Your Review:</p>
-                        <p class="card text-md-dark ">French elegance at its finest, this is one of Andersons' best work and completely captures the aestetic of rural France via it's depection of the fictional town "Enueui"</p>
-                    </div>
-                </div>
-
-                <div class="card align-middle m-5" style={{ width: "25rem" }}>
-                    <div class="card-body">
-                        <img src="https://m.media-amazon.com/images/M/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_.jpg" class="card-img-top rounded" style={{ height: "30rem" }} alt="..." />
-                        <h5 class="card-title display-5">Avengers Endgame</h5>
-                        <p class="card-text text-primary">Your Review:</p>
-                        <p class="card">Best marvel movie every made, need I say more!</p>
-                    </div>
-                </div>
-            </div>
-            <div class="d-inline-flex flex-row justify-content-center p-3">
-
-            <button type="button" class="btn-lg btn btn-primary m-4 p-3">Write a review</button>
-            <button type="button" class="btn-lg btn btn-success m-4 p-3">Toggle Next</button>
-
 
             </div>
             
